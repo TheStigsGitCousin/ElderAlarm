6b2c4e74d94956203dee2c022e88eedc75542d59
TheStigsGitCousin <david.i.fagerlund@gmail.com>
2015-04-15 11:58:52 +0200

    Some random changes

--- wear/src/main/java/com/app/crunchyonioncoolkit/elderalarm/PulseHandler.java
+++ wear/src/main/java/com/app/crunchyonioncoolkit/elderalarm/PulseHandler.java
@@ -5,9 +5,6 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.util.Log;
 
-import java.util.ArrayList;
-import java.util.Vector;
-
 /**
  * Created by David on 2015-04-14.
  */
@@ -15,39 +12,24 @@ public class PulseHandler implements SensorEventListener {
 
     private final String TAG = "PulseHandler";
 
-    protected static ArrayList<Event> eventListeners;
+    private static float heartRate;
+
+    public float getHeartRate() {
+        return heartRate;
+    }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         float heartRate = event.values[0];
+        this.heartRate = heartRate;
         Log.d(TAG, "heart rate: " + Float.toString(heartRate));
 
         DecisionMaker.pulseChange(heartRate);
-        fireEvent(event.values);
+
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-    }
-
-    public static void addEventListener(Event event) {
-        if (eventListeners == null)
-            eventListeners = new ArrayList<>();
-
-        eventListeners.add(event);
-    }
-
-    public static void removeEventListener(Event event) {
-        if (eventListeners != null)
-            eventListeners.remove(event);
-    }
-
-    private static void fireEvent(float[] values){
-        if(eventListeners!=null && !eventListeners.isEmpty()){
-            for(Event event : eventListeners){
-                event.onChange(values);
-            }
-        }
     }
 }
