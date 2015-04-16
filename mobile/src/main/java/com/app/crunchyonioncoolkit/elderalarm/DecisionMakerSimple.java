c3dd2333e37530be5e126c6c33e2693ad529337d
clouddestroyer <maengl@kth.se>
2015-04-16 15:58:19 +0200

    måste
    måste

---
+++ mobile/src/main/java/com/app/crunchyonioncoolkit/elderalarm/DecisionMakerSimple.java
@@ -0,0 +1,35 @@
+package com.app.crunchyonioncoolkit.elderalarm;
+
+import android.util.Log;
+import java.util.Date;
+
+/**
+ * Created by Jack on 2015-04-16.
+ */
+public class DecisionMakerSimple {
+    private static SlidingWindow window = new SlidingWindow();
+
+    public static void accelerationChange(float[] acceleration) {
+        double SMV = Math.sqrt((acceleration[0] * acceleration[0]) + (acceleration[1] * acceleration[1]) + (acceleration[2] * acceleration[2]));
+        Log.d("DecisionMaker", "New acceleration: " + Double.toString(SMV));
+        window.newValue(SMV);
+        //fallDetection();
+    }
+
+    public static boolean detectFall(){
+        double [] SMV = window.getValueArray();
+        Date [] Date = window.getTimeStampArray();
+        int sum = 0;
+        sum += AlgorithmsSimple.MPI(SMV)? Constants.MPI_SIMPLE_SCORE:0;
+        sum += AlgorithmsSimple.MGI(SMV)? Constants.MGI_SIMPLE_SCORE:0;
+        // HeartRate
+
+        //Print out data if threshold reached
+        if(sum >= Constants.FALL_SCORE_RESUALT_THRESHOLD ){
+            DataOut.simpleTestPrint(SMV, Date,"Simpel_Test_Data.txt");
+        }
+        return sum >= Constants.FALL_SCORE_RESUALT_THRESHOLD;
+        }
+    }
+
+
---
+++ mobile/src/main/java/com/app/crunchyonioncoolkit/elderalarm/SlidingWindow.java
@@ -0,0 +1,80 @@
+package com.app.crunchyonioncoolkit.elderalarm;
+
+import java.util.Date;
+import java.util.LinkedList;
+
+/**
+ * Created by David on 2015-04-14.
+ */
+public class SlidingWindow {
+
+    private LinkedList<Sample> window;
+
+    public SlidingWindow() {
+        window = new LinkedList<>();
+    }
+
+    public void newValue(double SMV) {
+        window.addLast(new Sample(SMV, new Date()));
+
+        if (window.size() > Constants.WINDOW_WIDTH) {
+            window.removeFirst();
+        }
+    }
+
+    public LinkedList<Sample> getWindow() {
+        return window;
+    }
+
+    public double[] getSamples(){
+
+
+    }
+
+    public double[] getArray(){
+
+        double[] array = new double[window.size()];
+        int i = 0;
+        for (Sample d : window) {
+            array[i] = d.value;
+            i++;
+        }
+        return array;
+
+    }
+
+    public double[] getValueArray(){
+
+        double[] array = new double[window.size()];
+        int i = 0;
+        for (Sample d : window) {
+            array[i] = d.value;
+            i++;
+        }
+        return array;
+
+    }
+
+    public Date[] getTimeStampArray(){
+
+        Date[] array = new Date[window.size()];
+        int i = 0;
+        for (Sample d : window) {
+            array[i] = d.timeStamp;
+            i++;
+        }
+        return array;
+
+    }
+
+    public class Sample{
+        public double value;
+        public Date timeStamp;
+        public Sample(double w, Date t){
+            value = w;
+            timeStamp = t;
+        }
+
+    }
+
+}
