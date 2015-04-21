f48e0d74dbfac24be3ece8de8af6c1d2fdbd1285
TheStigsGitCousin <david.i.fagerlund@gmail.com>
2015-04-21 12:12:58 +0200

    Begin rebuild of file IO

---
+++ wear/src/main/java/com/app/crunchyonioncoolkit/elderalarm/DataIO.java
@@ -0,0 +1,18 @@
+package com.app.crunchyonioncoolkit.elderalarm;
+
+import android.content.Context;
+
+/**
+ * Created by David on 2015-04-21.
+ */
+public class DataIO {
+
+    public static void writeToFile(Context context,String path, String data){
+
+    }
+
+    public static String reaadFromFile(Context context,String path){
+        context.
+    }
+
+}
