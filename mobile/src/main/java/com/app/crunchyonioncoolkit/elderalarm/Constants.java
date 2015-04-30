b7294796924519042b1c0a4562f9dd7f0fdb0c98
clouddestroyer <maengl@kth.se>
2015-04-30 15:26:15 +0200

    sdf
    sdf

--- mobile/src/main/java/com/app/crunchyonioncoolkit/elderalarm/Constants.java
+++ mobile/src/main/java/com/app/crunchyonioncoolkit/elderalarm/Constants.java
@@ -6,43 +6,43 @@ package com.app.crunchyonioncoolkit.elderalarm;
 public class Constants {
 
     // Threshold to determine peak time
-    public static double PEAK_TIME_THRESHOLD = 10;
+    public static double PEAK_TIME_THRESHOLD = 24.4;
     // Samples after peak when SMV < PEAK_TIME_THRESHOLD
     public static int TIME_WITHOUT_PEAKS = 2000;
     // Sliding window time size (ms)
     public static int WINDOW_WIDTH = 4000;
-    // Interval to search for impact end
-    public static int IMPACT_END_INTERVAL = 25;
+    // Interval to search for impact end (ms)
+    public static int IMPACT_END_INTERVAL = 1000;
     // Magnitude of impact
-    public static double IMPACT_END_MAGNITUDE_THRESHOLD = 0.34;
-    // Interval to search for impact start
-    public static int IMPACT_START_INTERVAL = 25;
+    public static double IMPACT_END_MAGNITUDE_THRESHOLD = 1.5;
+    // Interval to search for impact start (ms)
+    public static int IMPACT_START_INTERVAL = 1200;
     // Magnitude after impact
-    public static double IMPACT_START_LOW_THRESHOLD = 0.34;
+    public static double IMPACT_START_LOW_THRESHOLD = 1.5;
     // Win interval
-    public static int WIN_INTERVAL = 40;
-    //
+    public static int WIN_INTERVAL = 1000;
+    // AAMV
     public static double AAMV_THRESHOLD = 0.52;
-    public static int IDI_THRESHOLD = 20;
+    public static int IDI_THRESHOLD = 320;
     // Maximum acceleration
-    public static double MAXIMUM_PEAK_THRESHOLD = 5.8;
+    public static double MAXIMUM_PEAK_THRESHOLD = 102.4;
     // MVI start interval
-    public static int MVI_Interval = 10;
+    public static int MVI_Interval = 500;
     // MVI average magnitude interval
-    public static double MVI_AVERAGE_MAGNITUDE_LOW = 0.3;
+    public static double MVI_AVERAGE_MAGNITUDE_LOW = 0.24;
     // MVI average magnitude interval
-    public static double MVI_AVERAGE_MAGNITUDE_HIGH = 0.8;
+    public static double MVI_AVERAGE_MAGNITUDE_HIGH = 0.9;
     // PDI magnitude
-    public static int PDI_MAGNITUDE = 100;
+    public static int PDI_INTERVAL = 1291;
     public static double PDI_THRESHOLD = 0.8;
 
-    public static int ARI_INTERVAL = 35;
+    public static int ARI_INTERVAL = 700;
     public static double ARI_LOW = 0.85;
     public static double ARI_HIGH = 1.3;
-    public static double ARI_THRESHOLD = 0.6;
+    public static double ARI_THRESHOLD = 0.6; // Kolla skillnad mellan fall och slå sensorn
 
     public static double FFI_THRESHOLD = 0.8;
-    public static int FFI_INTERVAL = 10;
+    public static int FFI_INTERVAL = 200;
     public static double FFI_MINIMUM_FALL_THRESHOLD = 0.6;
 
 
